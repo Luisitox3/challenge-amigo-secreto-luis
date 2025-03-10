e1dcc56ae1b43cb9d662e3c90b6163b6f0e85f71
Luis Martinez <lamartinezp1p80@gmail.com>
2025-03-10 00:42:05 -0600

    Metodos y array

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres = [] 
+
+    document.addEventListener("DOMContentLoaded", function()){
+        const inputNombre = document.getElementById("inputNombre");
+        const btnAgregar = document.getElementById("btnAgregar");
+        const btnSeleccionar = document.getElementById("btnSeleccionar");
+        const listaNombres = document.getElementById("listaNombres"); 
+        
+
+    }
